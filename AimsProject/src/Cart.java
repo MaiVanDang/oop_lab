f6e841102781f950c792fd9aaa6b2b1a1f3d2b73
MaiVanDang <maivandang24@gmail.com>
2024-11-26 00:43:37 +0700

    lab3

--- AimsProject/src/Cart.java
+++ AimsProject/src/Cart.java
@@ -17,64 +17,6 @@ public class Cart {
         }
     }
     
-    // Phương thức thêm list đĩa video vào giỏ hàng 2.1a (method overloading)
-//    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
-//    	for(DigitalVideoDisc dvd : dvdList) {
-//    		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
-//    			itemsOrdered[qtyOrdered] = dvd;
-//                qtyOrdered++;
-//                System.out.println("The disc \"" + dvd.getTitle() + "\" has been added.");
-//                if (qtyOrdered == MAX_NUMBERS_ORDERED) {
-//                    System.out.println("The cart is almost full!");
-//                    return;
-//                }
-//    		} else {
-//    			System.out.println("The cart is full! Cannot add more items.");
-//    			return;
-//    		}
-//    	}
-//    }
-    
-    // Phương thức thêm list đĩa video vào giỏ hàng 2.1b (method overloading)
-    public void addDigitalVideoDisc(DigitalVideoDisc... dvdList) {
-    	for(DigitalVideoDisc dvd : dvdList) {
-    		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
-    			itemsOrdered[qtyOrdered] = dvd;
-                qtyOrdered++;
-                System.out.println("The disc \"" + dvd.getTitle() + "\" has been added.");
-                if (qtyOrdered == MAX_NUMBERS_ORDERED) {
-                    System.out.println("The cart is almost full!");
-                    return;
-                }
-    		} else {
-    			System.out.println("The cart is full! Cannot add more items.");
-    			return;
-    		}
-    	}
-    }  
-    
-    // Phương thức thêm 2 đĩa video vào giỏ hàng 2.2 (method overloading)
-    public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2) {
-        if(qtyOrdered + 2 <= MAX_NUMBERS_ORDERED) {
-        	itemsOrdered[qtyOrdered] = dvd1;
-            qtyOrdered++;
-            System.out.println("The disc \"" + dvd1.getTitle() + "\" has been added.");
-            itemsOrdered[qtyOrdered] = dvd2;
-            qtyOrdered++;
-            System.out.println("The disc \"" + dvd2.getTitle() + "\" has been added.");
-            if (qtyOrdered == MAX_NUMBERS_ORDERED) {
-                System.out.println("The cart is almost full!");
-            }
-        }else if (qtyOrdered + 1 == MAX_NUMBERS_ORDERED) {
-        	itemsOrdered[qtyOrdered] = dvd1;
-            qtyOrdered++;
-            System.out.println("The disc \"" + dvd1.getTitle() + "\" has been added.");
-            System.out.println("The cart is almost full!");
-        }else {
-        	System.out.println("The cart is full! Cannot add more items.");
-        }
-    }
-    
     // Phương thức xóa đĩa video khỏi giỏ hàng
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         for (int i = 0; i < qtyOrdered; i++) {
